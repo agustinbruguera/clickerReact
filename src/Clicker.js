d0cd6b19fa2a95a0ba7760fa2bb6354f059308af
agustinbruguera <agustinbruguera@gmail.com>
2022-01-27 14:55:17 -0300

    Botones incrementales

--- src/Clicker.js
+++ src/Clicker.js
@@ -1,9 +1,11 @@
 import { Button, Container, Col, Row } from 'react-bootstrap';
 import React, { useState } from 'react';
+import { useButton } from './useButton';
 
 export const Clicker = () => {
 
-  const [counter, setCounter] = useState(0);
+
+  const {counter, increment, increment1} = useButton(20);
 
 
   return (
@@ -15,26 +17,28 @@ export const Clicker = () => {
             <h1 className='text-center'>Clicker</h1>
           </Col>
         </Row>
+
         <Row>
           <Col>
-            <h2 className='text-center'> {counter} </h2>
+            <h2 className='text-center'> { counter } </h2>
           </Col>
         </Row>
+        
         <Row>
           <div className="d-grid gap-1">
-            <Button variant="primary" size="md"> +1  </Button>
+            <Button onClick={ increment1 } variant="primary" size="md"> +1  </Button>
 
-            <Button variant="secondary" size="md"> +5 </Button>
+            <Button onClick={ () => increment(5) } variant="secondary" size="md"> +5 </Button>
 
-            <Button variant="success" size="md"> +10 </Button>
+            <Button onClick={ () => increment(10) } variant="success" size="md"> +10 </Button>
 
-            <Button variant="warning" size="md"> +25  </Button>
+            <Button onClick={ () => increment(25) } variant="warning" size="md"> +25  </Button>
+ 
+            <Button onClick={ () => increment(50) } variant="danger" size="md"> +50 </Button>
 
-            <Button variant="danger" size="md"> +50 </Button>
+            <Button onClick={ () => increment(100) } variant="info" size="md"> +100 </Button>
 
-            <Button variant="info" size="md"> +100 </Button>
-
-            <Button variant="dark" size="md"> +200 </Button>
+            <Button onClick={ () => increment(200) } variant="dark" size="md"> +200 </Button>
           </div>
 
         </Row>
--- src/useButton.js
+++ src/useButton.js
@@ -1,13 +1,26 @@
 import React, {useState} from 'react';
 
-export const useButton = () => {
+export const useButton = (initialState = 10) => {
+    
+    const [counter, setCounter] = useState(initialState);
 
-    const [factor, setFactor] = useState(1);
-
-    const increment = () => {
-
-        setFactor
+    const increment1 = () => {
+        console.log("Boton 1 ")
+        setCounter(counter + 1)
     }
 
+    const increment = ( factor = 1 ) => {
 
+        console.log(`Boton ${factor}`)
+        setCounter(counter + factor);
+    }
+
+    
+
+    
+    return{
+        counter,
+        increment,
+        increment1,
+    }
 };
