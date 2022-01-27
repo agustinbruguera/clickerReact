70fcb82dc275520243f7cacff86f7e5bafdd8d09
agustinbruguera <agustinbruguera@gmail.com>
2022-01-27 15:43:10 -0300

    Botones hide/show, Toast al llegar a 5000

--- src/Clicker.js
+++ src/Clicker.js
@@ -1,15 +1,17 @@
-import { Button, Container, Col, Row } from 'react-bootstrap';
+import { Button, Container, Col, Row, ToastContainer, Toast } from 'react-bootstrap';
 import React, { useState } from 'react';
 import { useButton } from './useButton';
 
 export const Clicker = () => {
 
 
-  const {counter, increment, increment1} = useButton(20);
+  const { counter, increment, increment1 } = useButton(0);
 
 
   return (
     <>
+
+
 
       <Container fluid>
         <Row>
@@ -20,41 +22,42 @@ export const Clicker = () => {
 
         <Row>
           <Col>
-            <h2 className='text-center'> { counter } </h2>
+            <h2 className='text-center'> {counter} </h2>
           </Col>
         </Row>
-        
+
         <Row>
           <div className="d-grid gap-1">
-            <Button onClick={ increment1 } variant="primary" size="md"> +1  </Button>
+            <Button onClick={increment1} variant="primary" size="md"> +1  </Button>
 
-            <Button onClick={ () => increment(5) } variant="secondary" size="md"> +5 </Button>
+            {(counter >= 10) && (<Button onClick={() => increment(5)} variant="secondary" size="md"> +5 </Button>)}
 
-            <Button onClick={ () => increment(10) } variant="success" size="md"> +10 </Button>
+            {(counter >= 25) && (<Button onClick={() => increment(10)} variant="success" size="md"> +10 </Button>)}
 
-            <Button onClick={ () => increment(25) } variant="warning" size="md"> +25  </Button>
- 
-            <Button onClick={ () => increment(50) } variant="danger" size="md"> +50 </Button>
+            {(counter >= 100) && (<Button onClick={() => increment(25)} variant="warning" size="md"> +25  </Button>)}
 
-            <Button onClick={ () => increment(100) } variant="info" size="md"> +100 </Button>
+            {(counter >= 250) && (<Button onClick={() => increment(50)} variant="danger" size="md"> +50 </Button>)}
 
-            <Button onClick={ () => increment(200) } variant="dark" size="md"> +200 </Button>
+            {(counter >= 500) && (<Button onClick={() => increment(100)} variant="info" size="md"> +100 </Button>)}
+
+            {(counter >= 1000) && (<Button onClick={() => increment(200)} variant="dark" size="md"> +200 </Button>)}
           </div>
 
         </Row>
       </Container>
 
-
-
-
-
-
-
-
-
-
-
-
+      {counter >= 5000 &&
+        (<ToastContainer position="top-end">
+          <Toast bg="success">
+            <Toast.Header>
+              <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+              <strong className="me-auto">Bootstrap</strong>
+              <small className="text-muted">just now</small>
+            </Toast.Header>
+            <Toast.Body>Felcidades Pai</Toast.Body>
+          </Toast>
+        </ToastContainer>)
+      }
 
 
     </>
